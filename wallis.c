b422d3ca78bc9afa947277b1b07730263bdc33c8
ATHULYA011 <85112353+ATHULYA011@users.noreply.github.com>
2021-06-04 21:10:06 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -1,20 +1,31 @@
-#include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 float wallis_pi(int);
+int main(void) {
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
 
-{float pi=1.0;
-  for (int i=1;i<n; i++)
-{
-int m;
-m=4.0*i*i;
-pi=(float) ((pi*m)/(m-1));
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
 }
-pi=pi *2.0;
-return pi;
+float wallis_pi(int n){
+  int i;
+  float k,ans=1.0;
+  for(i=1;i<=n;i++){
+    k=(float)(4*i*i)/(4*i*i-1);
+    ans=ans*k;
+  }
+  return(2*ans);
 }
-
-
-
